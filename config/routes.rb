d2a85b1f9042a68db1be3aafb05e35577498737f
jared ables <jaredables@gmail.com>
2023-07-17 16:39:47 -0600

    sample turbo app

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+Rails.application.routes.draw do
+  root 'tasks#index'
+  post 'tasks/:id/toggle', to: 'tasks#toggle'
+
+  resources :tasks
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
---
+++ db/migrate/20230717145831_create_tasks.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class CreateTasks < ActiveRecord::Migration[7.0]
+  def change
+    create_table :tasks do |t|
+      t.string :description, limit: 200
+      t.boolean :completed
+
+      t.timestamps
+    end
+  end
+end
